7ce0d33c38f3c7d13863bfb6ad86f1fedc49af88
Daniil Sokolov <sokolov.2304@yandex.ru>
2023-04-14 13:12:13 +0400

    first_commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # qa_python
+Проверки:
+
+Добавление книг - test_add_new_book
+
+Проверить что нельзя дважды добавить книгу - test_you_cant_add_same_book_one_more_time
+
+Недоступно выставление рейтинга книге не в списке - test_lack_of_rating_of_a_book_that_is_not_in_the_list
+
+Нельзя поставить рейтинг < 1 - test_rating_less_one
+
+Нельзя поставить рейтинг > 10 - test_rating_more_ten
+
+Отсутствие рейгинга у книги, которая не добавлена - test_there_is_no_rating_for_a_non_existent_book
+
+Добавление книги в избранное - test_add_to_favorites
+
+Если книги в словаре нет, то и добаить в избранное её нельзя - test_book_no_favorites
+
+Проверка удаления книги - test_deleting_a_book_from_favorites
--- tests.py
+++ tests.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -22,3 +25,57 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book(self, collector):
+        collector = BooksCollector()
+        collector.add_new_book("Интересная книга")
+        assert len(collector.get_books_rating()) == 1, "Книга не добавилась"
+    def test_you_cant_add_same_book_one_more_time(self, collector, White_Bim_Black_ear, Kolobok):
+        collector = BooksCollector()
+        assert len(collector.get_books_rating()) == 2, "Число книг не рано двум"
+    def test_lack_of_rating_of_a_book_that_is_not_in_the_list(self):
+        collector = BooksCollector()
+        collector.set_book_rating("Ни ной", 8)
+        assert collector.get_books_rating() == {'Белый Бим чёрное ухо': 1}
+    def test_rating_less_one(self, collector):
+        collector = BooksCollector()
+        collector.add_new_book("Любая книга")
+        collector.set_book_rating("Любая книга", 0)
+        collector.set_book_rating("Любая книга", 5)
+        assert collector.get_books_rating("Любая книга")== 5, "Доступен рейтинг < 1"
+    def test_rating_more_ten(self, collector,White_Bim_Black_ear):
+        collector = BooksCollector()
+        collector.set_book_rating(list(collector.books_rating())[0], 11)
+        assert list(collector.books_rating.values())[0] == 1, "Доступен рейтинг > 10"
+    def test_there_is_no_rating_for_a_non_existent_book(self, collector):
+        collector = BooksCollector()
+        collector.add_new_book('Маша и медведь')
+        collector.set_book_rating("Ни ной", 2)
+        assert 'Ни ной' not in collector.get_list_of_favorites_books(), "Присвоен рейтинг книге, которая не добавлена"
+    def test_add_to_favorites(self, collector):
+        collector = BooksCollector()
+        collector.add_new_book("Книга")
+        collector.set_book_rating("Книга")
+        assert len(collector.get_list_of_favorites_books()) == 1
+        assert "Книга" in collector.get_list_of_favorites_books(), 'Не добавлена в избранное'
+
+    def test_book_no_favorites(self, collector):
+        collector.add_book_in_favorites('Три поросёнка')
+        assert 'Три поросёнка' not in collector.favorites, "Добавление в избранное не существующей книги"
+
+    def test_deleting_a_book_from_favorites(self, collector, White_Bim_Black_ear):
+        collector = BooksCollector()
+        collector.add_book_in_favorites(list(collector.get_books_rating.keys())[0])
+        collector.delete_book_from_favorites(list(collector.books_rating.keys())[0])
+        collector.get_list_of_favorites_books()
+        assert len(collector.get_list_of_favorites_books()) == 0, "Книга из избранного не удалена"
+    def test_getting_a_book_with_the_right_rating(self, collector, White_Bim_Black_ear, Kolobok, Ni_noy):
+        collector = BooksCollector()
+        collector.set_book_rating(list(collector.books_rating.keys())[0], 1)
+        collector.set_book_rating(list(collector.books_rating.keys())[1], 6)
+        collector.set_book_rating(list(collector.books_rating.keys())[2], 10)
+        assert len(collector.get_books_with_specific_rating(1)) == 1, "Книги с не правильным рейтингом"
+    def test_set_book_rating(self, White_Bim_Black_ear):
+        collector = BooksCollector()
+        collector.add_new_book("Вини Пух")
+        collector.set_book_rating("Вини Пух", 9)
+        assert collector.books_rating.get("Вини Пух") == 9, "Рейтинг книги не соотвествует"
